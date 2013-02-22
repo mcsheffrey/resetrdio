5a3adc33087346240bbbd56a8ba2f13af357716b
Connor McSheffrey <connor.mcsheffrey@gmail.com>
2013-02-22 08:11:19 -0800

    Looping through each type of ownership for playlists and either uncollaborating or removing the playlist from collection

--- reset_rdio.py
+++ reset_rdio.py
@@ -1,12 +1,36 @@
 from rdio import Rdio
+from rdio_consumer_credentials import *
+from urllib2 import HTTPError
 
-rdio = Rdio(("gjdmsspd3tzpu9nesqn6xm37", "683h9QUSX3"))
+# create an instance of the Rdio object with our consumer credentials
+rdio = Rdio((RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET))
 
-ian = rdio.call("findUser", {"vanityName": "connormc"})
-if (ian["status"] == "ok"):
-	print ian["result"]
-else:
-  print "ERROR: " + ian["message"]
+try:
+  # authenticate against the Rdio service
+  url = rdio.begin_authentication('oob')
+  print 'Go to: ' + url
+  verifier = raw_input('Then enter the code: ').strip()
+  rdio.complete_authentication(verifier)
 
-playlists = rdio.call("getPlaylists", {"user": "s93580"})
-print playlists
+  # find out what playlists you created
+  ownedPlaylists = rdio.call('getPlaylists')['result']['owned']
+  collabPlaylists = rdio.call('getPlaylists')['result']['collab']
+  subscribedPlaylists = rdio.call('getPlaylists')['result']['subscribed']
+
+  # list them
+  for playlist in ownedPlaylists:
+    print '%(shortUrl)s\t%(name)s' % playlist
+    print playlist['key']
+
+  for playlist in collabPlaylists:
+    print '%(shortUrl)s\t%(name)s' % playlist
+    rdio.call('setPlaylistCollaborating', {'playlist': playlist['key'], 'collaborating': 'false'})
+
+  for playlist in subscribedPlaylists:
+    print '%(shortUrl)s\t%(name)s' % playlist
+    rdio.call('removeFromCollection', {'keys': playlist['key']})
+
+
+except HTTPError, e:
+  # if we have a protocol error, print it
+  print e.read()
